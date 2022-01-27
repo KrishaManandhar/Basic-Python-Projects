56153b3ed521bf54d0afdce47bb4300f72189a39
KrishaManandhar <98373496+KrishaManandhar@users.noreply.github.com>
2022-01-27 21:30:44 +0545

    file added

--- MADLIB/madlib.py
+++ MADLIB/madlib.py
@@ -1,1 +1,13 @@
+# MADLIB
+person_name = input("Enter person name: ")
+drinks = input("enter any drinks: ")
+verb = input("Enter a verb: ")
+animal = input("Enter an animal: ")
+noun = input("Enter your noun: ")
 
+madlib = f"{person_name} drank {drinks} alot \
+{person_name} had a great {verb} \
+all the {person_name}'s {animal} and {person_name}'s {noun} \
+Couldn't put {person_name} together again"
+
+print(madlib)
